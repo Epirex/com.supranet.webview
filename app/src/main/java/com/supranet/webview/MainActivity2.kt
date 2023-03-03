6ccdf0a77cd17391781b9f88ae438e612bbe925b
Epirex <estebanguzzo@gmail.com>
2023-03-03 10:01:16 -0300

    Save data of EditText

--- app/src/main/java/com/supranet/webview/MainActivity2.kt
+++ app/src/main/java/com/supranet/webview/MainActivity2.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.supranet.webview.databinding.ActivityMain2Binding
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -15,10 +17,21 @@ class MainActivity2 : AppCompatActivity() {
 
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val campo=findViewById<EditText>(R.id.edittext)
+
         val button2=findViewById<Button>(R.id.button2)
-        button2.setOnClickListener {
-            val intento2 = Intent(this, MainActivity::class.java)
-            startActivity(intento2)
+        //button2.setOnClickListener {
+            //val intento2 = Intent(this, MainActivity::class.java)
+            //startActivity(//intento2)
+            // Setting On Click Listener
+            button2.setOnClickListener {
+
+                // Guardar texto en variable
+                val text = campo.text
+
+                // Mostrar url temporalmente
+                Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+            }
         }
     }
-}
