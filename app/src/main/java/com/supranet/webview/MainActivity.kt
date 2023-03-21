4b603384db371b0c4a604c8790d55d169e20f881
Epirex <estebanguzzo@gmail.com>
2023-03-21 17:55:23 -0300

    Set icon and full screen

--- app/src/main/java/com/supranet/webview/MainActivity.kt
+++ app/src/main/java/com/supranet/webview/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.supranet.webview
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.util.AttributeSet
+import android.view.*
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.FrameLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.preference.PreferenceManager
+import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,6 +38,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Pantalla completa
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
         webView = findViewById(R.id.webview)
         webView.webViewClient = WebViewClient()
 
@@ -44,10 +55,4 @@ class MainActivity : AppCompatActivity() {
         val urlPreference = sharedPrefs.getString("url_preference", "https://www.google.com")
         webView.loadUrl(urlPreference.toString())
     }
-
-    //val myWebView: WebView = findViewById(R.id.webview)
-    //myWebView.setWebViewClient(WebViewClient())
-    //myWebView.settings.javaScriptEnabled = true
-    //val dato = intent.getStringExtra("direccion")
-    //myWebView.loadUrl("https://${dato}")
 }
--- app/src/main/java/com/supranet/webview/SettingsActivity.kt
+++ app/src/main/java/com/supranet/webview/SettingsActivity.kt
@@ -16,6 +16,12 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportFragmentManager.beginTransaction().replace(android.R.id.content, SettingsFragment()).commit()
+
+        // Pantalla completa
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
