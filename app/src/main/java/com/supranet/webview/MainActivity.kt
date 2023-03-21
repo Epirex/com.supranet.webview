27647f2a56c32f6e0a98ff6b40a8962d1d3ff38b
Esteban Guzzo <estebanguzzo@gmail.com>
2023-03-21 14:49:42 -0300

    Rebuild webview and disable features
    Signed-off-by: Esteban Guzzo <estebanguzzo@gmail.com>

--- app/src/main/java/com/supranet/webview/MainActivity.kt
+++ app/src/main/java/com/supranet/webview/MainActivity.kt
@@ -3,8 +3,11 @@ package com.supranet.webview
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.preference.PreferenceManager
@@ -13,6 +16,21 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,57 +39,19 @@ class MainActivity : AppCompatActivity() {
         webView = findViewById(R.id.webview)
         webView.webViewClient = WebViewClient()
 
-        loadUrlFromPreferences()
+        // Configurar WebView
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+
+        // Cargar URL
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val urlPreference = sharedPrefs.getString("url_preference", "https://www.google.com")
+        webView.loadUrl(urlPreference.toString())
     }
 
-    private fun loadUrlFromPreferences() {
-        val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        val url: String? = sharedPreferences.getString("url", "https://www.ejemplo.com")
-        webView.loadUrl(url!!)
-
-        //val myWebView: WebView = findViewById(R.id.webview)
-        //myWebView.setWebViewClient(WebViewClient())
-        //myWebView.settings.javaScriptEnabled = true
-        //val dato = intent.getStringExtra("direccion")
-        //myWebView.loadUrl("https://${dato}")
-
-        // floating button
-        val button1: View = findViewById(R.id.floatingActionButton)
-        button1.setOnClickListener { view ->
-            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .show()
-        }
-        button1.setOnClickListener {
-            val intento1 = Intent(this, SettingsActivity::class.java)
-            startActivity(intento1)
-            }
-        }
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) hideSystemUI()
-    }
-
-    private fun hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                // Set the content to appear under the system bars so that the
-                // content doesn't resize when the system bars hide and show.
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // Hide the nav bar and status bar
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
-    }
-
-    // Shows the system bars by removing all the flags
-    // except for the ones that make the content appear under the system bars.
-    private fun showSystemUI() {
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
-    }
-    }
+    //val myWebView: WebView = findViewById(R.id.webview)
+    //myWebView.setWebViewClient(WebViewClient())
+    //myWebView.settings.javaScriptEnabled = true
+    //val dato = intent.getStringExtra("direccion")
+    //myWebView.loadUrl("https://${dato}")
+}
--- app/src/main/java/com/supranet/webview/SettingsActivity.kt
+++ app/src/main/java/com/supranet/webview/SettingsActivity.kt
@@ -15,77 +15,24 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
-        //setting toolbar
-        setSupportActionBar(findViewById(R.id.my_toolbar))
-
-        //home navigation
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.settings, SettingsFragment())
-                    .commit()
-        }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) hideSystemUI()
-    }
-
-    private fun hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                // Set the content to appear under the system bars so that the
-                // content doesn't resize when the system bars hide and show.
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // Hide the nav bar and status bar
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
-    }
-
-    // Shows the system bars by removing all the flags
-    // except for the ones that make the content appear under the system bars.
-    private fun showSystemUI() {
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        supportFragmentManager.beginTransaction().replace(android.R.id.content, SettingsFragment()).commit()
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
         }
-    }
-    class SettingsActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.xml.root_preferences)
-
-            // ...
-
-            val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-            sharedPreferences.registerOnSharedPreferenceChangeListener(this)
-        }
-
-        override fun onDestroy() {
-            super.onDestroy()
-
-            val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-            sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
-        }
-
-        override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-            if (key == "url") {
-                //loadUrlFromPreferences()
+         override fun onPreferenceTreeClick(preference: Preference): Boolean {
+            if (preference?.key == "url_preference") {
+                val editTextPreference = preference as EditTextPreference
+                editTextPreference.setOnPreferenceChangeListener { preference, newValue ->
+                    val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+                    sharedPrefs.edit().putString("url_preference", newValue.toString()).apply()
+                    true
+                }
             }
+            return super.onPreferenceTreeClick(preference)
         }
     }
 }
