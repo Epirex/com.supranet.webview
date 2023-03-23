afb2835ccb79613dbc857831e0cf7785de0a8660
Epirex <estebanguzzo@gmail.com>
2023-03-23 13:01:27 -0300

    Add show toolbar option (w.i.p)

--- app/src/main/java/com/supranet/webview/MainActivity.kt
+++ app/src/main/java/com/supranet/webview/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.supranet.webview
 
+import androidx.appcompat.app.ActionBar
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -12,6 +17,19 @@ import androidx.preference.PreferenceManager
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
+    private lateinit var actionBar: ActionBar
+    private lateinit var preferences: SharedPreferences
+
+    private val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+        if (key == "show_window_action_bar") {
+            if (preferences.getBoolean("show_window_action_bar", false)) {
+                showActionBar()
+            } else {
+                hideActionBar()
+            }
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
@@ -54,11 +72,44 @@ class MainActivity : AppCompatActivity() {
                 show()
             }
         }
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        actionBar = supportActionBar!!
+
+        preferences.registerOnSharedPreferenceChangeListener(listener)
+
+        if (preferences.getBoolean("show_toolbar", false)) {
+            showActionBar()
+        }
+
+        findViewById<View>(android.R.id.content).setOnTouchListener { _, _ ->
+            if (preferences.getBoolean("show_toolbar", false)) {
+                showActionBar()
+            }
+            true
+        }
     }
+
+    override fun onDestroy() {
+        preferences.unregisterOnSharedPreferenceChangeListener(listener)
+        super.onDestroy()
+    }
+
+    private fun showActionBar() {
+        actionBar.show()
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({ hideActionBar() }, 3000)
+    }
+
+    private fun hideActionBar() {
+        actionBar.hide()
+    }
+
+}
 
     //val myWebView: WebView = findViewById(R.id.webview)
     //myWebView.setWebViewClient(WebViewClient())
     //myWebView.settings.javaScriptEnabled = true
     //val dato = intent.getStringExtra("direccion")
     //myWebView.loadUrl("https://${dato}")
-}
