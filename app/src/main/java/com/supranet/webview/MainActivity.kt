a70000924bca755a6f08b700f6b2b7cd098a548a
Epirex <estebanguzzo@gmail.com>
2023-03-23 17:16:00 -0300

    Update fullscreen

--- app/src/main/java/com/supranet/webview/MainActivity.kt
+++ app/src/main/java/com/supranet/webview/MainActivity.kt
@@ -85,15 +85,34 @@ class MainActivity : AppCompatActivity() {
         val preferences = PreferenceManager.getDefaultSharedPreferences(this)
         val fullscreenPreference = preferences.getBoolean("fullscreen", false)
 
-        if (preferences.getBoolean("fullscreen", false)) {
-            // Habilita la pantalla completa
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
+        fun hideSystemUI() {
+            // Enables regular immersive mode.
+            // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+            // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    // Hide the nav bar and status bar
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
+
+        // Shows the system bars by removing all the flags
+        // except for the ones that make the content appear under the system bars.
+        fun showSystemUI() {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        }
+
+        // Habilita o deshabilita el modo pantalla completa
+        if (fullscreenPreference) {
+            hideSystemUI()
         } else {
-            // Deshabilita la pantalla completa si la opción no está marcada
-            window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            showSystemUI()
         }
     }
 }
