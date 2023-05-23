228378f3d62f01b29bea6df9ef289c28160dd6f0
Epirex <estebanguzzo@gmail.com>
2023-05-23 11:11:50 -0300

    Add funtion to send SVG with gmail api

--- app/src/main/java/com/supranet/webview/MainActivity.kt
+++ app/src/main/java/com/supranet/webview/MainActivity.kt
@@ -1,30 +1,30 @@
 package com.supranet.webview
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
-import android.os.Bundle
-import android.os.Environment
-import android.os.Handler
-import android.os.Looper
+import android.os.*
+import android.util.Base64
+import android.view.*
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.io.File
 import java.io.FileOutputStream
-import java.io.InputStream
 import java.text.SimpleDateFormat
 import java.util.*
-import android.util.Base64
-import android.view.*
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.core.content.ContextCompat.startActivity
+import javax.activation.*
+import javax.mail.*
+import javax.mail.Message
+import javax.mail.internet.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -102,16 +102,17 @@ class MainActivity : AppCompatActivity() {
         // Configurar un WebViewClient para inyectar el CSS personalizado en cada página web cargada
         webView.webViewClient = object : WebViewClient() {
 
-                override fun onPageFinished(view: WebView?, url: String?) {
-                    injectCSS()
-                }
+            override fun onPageFinished(view: WebView?, url: String?) {
+                injectCSS()
+            }
+
             private fun injectCSS() {
                 try {
                     val inputStream = assets.open("looka.css")
                     val buffer = ByteArray(inputStream.available())
                     inputStream.read(buffer)
                     inputStream.close()
-                    val encoded = Base64.encodeToString(buffer , Base64.NO_WRAP)
+                    val encoded = Base64.encodeToString(buffer, Base64.NO_WRAP)
                     webView.loadUrl(
                         "javascript:(function() {" +
                                 "var parent = document.getElementsByTagName('head').item(0);" +
@@ -137,7 +138,13 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "El archivo HTML no existe!", Toast.LENGTH_SHORT).show()
                 return
             }
-            webView.loadDataWithBaseURL("file://${file.parent}/", file.readText(), "text/html", "UTF-8", null)
+            webView.loadDataWithBaseURL(
+                "file://${file.parent}/",
+                file.readText(),
+                "text/html",
+                "UTF-8",
+                null
+            )
         }
 
         // check toolbar
@@ -152,29 +159,35 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Agregar el listener onTouch para mostrar u ocultar la ActionBar
-        val gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
-            override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-                // Obtiene las dimensiones de la pantalla
-                val displayMetrics = resources.displayMetrics
-                val screenHeight = displayMetrics.heightPixels
+        val gestureDetector =
+            GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
+                override fun onFling(
+                    e1: MotionEvent,
+                    e2: MotionEvent,
+                    velocityX: Float,
+                    velocityY: Float
+                ): Boolean {
+                    // Obtiene las dimensiones de la pantalla
+                    val displayMetrics = resources.displayMetrics
+                    val screenHeight = displayMetrics.heightPixels
 
-                // Define la región del borde superior de la pantalla
-                val topRegion = 50 // En píxeles
+                    // Define la región del borde superior de la pantalla
+                    val topRegion = 50 // En píxeles
 
-                // Verifica si la posición inicial del evento se encuentra dentro de la región del borde superior de la pantalla
-                if (e1?.y ?: 0f < topRegion && e2?.y ?: 0f >= topRegion) {
-                    // Muestra el ActionBar
-                    supportActionBar?.show()
+                    // Verifica si la posición inicial del evento se encuentra dentro de la región del borde superior de la pantalla
+                    if (e1?.y ?: 0f < topRegion && e2?.y ?: 0f >= topRegion) {
+                        // Muestra el ActionBar
+                        supportActionBar?.show()
 
-                    // Oculta el ActionBar después de 3 segundos
-                    Handler().postDelayed({
-                        supportActionBar?.hide()
-                    }, 2500)
+                        // Oculta el ActionBar después de 3 segundos
+                        Handler().postDelayed({
+                            supportActionBar?.hide()
+                        }, 2500)
+                    }
+
+                    return super.onFling(e1, e2, velocityX, velocityY)
                 }
-
-                return super.onFling(e1, e2, velocityX, velocityY)
-            }
-        })
+            })
 
         // Asigna el GestureDetector al WebView
         webView.setOnTouchListener { _, event ->
@@ -195,22 +208,43 @@ class MainActivity : AppCompatActivity() {
             }
         }, refreshInterval * 60 * 1000L)
 
+        // Boton para descargar y enviar el archivo SVG
         saveButton = findViewById(R.id.save_button)
         saveButton.setOnClickListener {
-                webView.evaluateJavascript(
-                    "(function() { return encodeURIComponent(new XMLSerializer().serializeToString(document.querySelector('svg'))); })();"
-                ) { svg ->
-                    val decodedSvg = Uri.decode(svg)
-                    val contentSvg = decodedSvg.substring(1, decodedSvg.length - 1)
-                    val date = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
-                    val fileName = "logo_$date.svg"
-                    val file = File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), fileName)
-                    val outputStream = FileOutputStream(file)
-                    outputStream.write(contentSvg.toByteArray(Charsets.UTF_8))
-                    outputStream.close()
-                    Toast.makeText(applicationContext, "Archivo guardado: $fileName", Toast.LENGTH_SHORT).show()
+            webView.evaluateJavascript(
+                "(function() { return encodeURIComponent(new XMLSerializer().serializeToString(document.querySelector('svg'))); })();"
+            ) { svg ->
+                val decodedSvg = Uri.decode(svg)
+                val contentSvg = decodedSvg.substring(1, decodedSvg.length - 1)
+                val date = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+                val fileName = "logo_$date.svg"
+                val file = File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), fileName)
+                val outputStream = FileOutputStream(file)
+                outputStream.write(contentSvg.toByteArray(Charsets.UTF_8))
+                outputStream.close()
+                Toast.makeText(
+                    applicationContext,
+                    "Archivo guardado: $fileName",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                // Ventana de correo
+                val alertDialogBuilder = AlertDialog.Builder(this)
+                alertDialogBuilder.setTitle("¡Que buen logo! ahora te lo enviaremos por correo")
+                alertDialogBuilder.setMessage("Ingresa tu dirección de correo electrónico:")
+                val input = EditText(this)
+                alertDialogBuilder.setView(input)
+                alertDialogBuilder.setPositiveButton("Enviar") { dialog, _ ->
+                    val emailAddress = input.text.toString()
+                    SendEmailTask(emailAddress, file).execute()
+                    dialog.dismiss()
                 }
+                alertDialogBuilder.setNegativeButton("Cancelar") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                alertDialogBuilder.show()
             }
+        }
 
     // Crear el cuadro flotante
     passwordDialog = Dialog(this)
@@ -277,6 +311,87 @@ class MainActivity : AppCompatActivity() {
             passwordDialog.dismiss()
         }
 }
+
+    // Envio del correo electronico en segundo plano
+    private inner class SendEmailTask(private val emailAddress: String, private val file: File) :
+        AsyncTask<Void, Void, Boolean>() {
+
+        override fun doInBackground(vararg params: Void?): Boolean {
+            return try {
+                sendEmailWithAttachment(emailAddress, file)
+                true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
+            }
+        }
+
+        override fun onPostExecute(success: Boolean) {
+            if (success) {
+                Toast.makeText(applicationContext, "Correo electrónico enviado", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Error al enviar el correo electrónico", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun sendEmailWithAttachment(emailAddress: String, file: File) {
+        val props = Properties()
+        props["mail.smtp.host"] = "smtp.gmail.com"
+        props["mail.smtp.port"] = "465"
+        props["mail.smtp.auth"] = "true"
+        props["mail.smtp.ssl.enable"] = "true"
+
+        // Remitente
+        val username = "supranet.logos@gmail.com"
+        val password = "npmtportarqirmyk"
+
+        val session = Session.getInstance(props, object : Authenticator() {
+            override fun getPasswordAuthentication(): PasswordAuthentication {
+                return PasswordAuthentication(username, password)
+            }
+        })
+
+        try {
+            val message = MimeMessage(session)
+            message.setFrom(InternetAddress(username))
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailAddress))
+            message.subject = "Supranet: ¡Tu nuevo logo ya esta aqui!"
+
+            // No me pregunten que es esto, lo saque de stackoverflow y me permitio enviar el archivo SVG
+            val mc = CommandMap.getDefaultCommandMap() as MailcapCommandMap
+            mc.addMailcap("text/html;; x-java-content-handler=com.sun.mail.handlers.text_html")
+            mc.addMailcap("text/xml;; x-java-content-handler=com.sun.mail.handlers.text_xml")
+            mc.addMailcap("text/plain;; x-java-content-handler=com.sun.mail.handlers.text_plain")
+            mc.addMailcap("multipart/*;; x-java-content-handler=com.sun.mail.handlers.multipart_mixed")
+            mc.addMailcap("message/rfc822;; x-java-content-handler=com.sun.mail.handlers.message_rfc822")
+            CommandMap.setDefaultCommandMap(mc)
+
+            val multipart = MimeMultipart()
+            val messageBodyPart = MimeBodyPart()
+            messageBodyPart.setText("¡Ya tienes disponible tu logo personalizado! Recuerda que tu logo se guardo en formato SVG, el mismo puede ser utilizado en cualquier editor (Recomendamos utilizar Canva por su facil usabilidad) Si deseas visualizar tu logo ahora mismo puedes hacerlo desde tu navegador favorito.")
+            multipart.addBodyPart(messageBodyPart)
+
+            val attachmentBodyPart = MimeBodyPart()
+            val fileDataSource = FileDataSource(file)
+            attachmentBodyPart.dataHandler = DataHandler(fileDataSource)
+            attachmentBodyPart.fileName = MimeUtility.encodeText(file.name)
+            multipart.addBodyPart(attachmentBodyPart)
+            message.setContent(multipart)
+
+            // Enviar el mensaje de correo electrónico
+            Transport.send(message)
+
+            runOnUiThread {
+                Toast.makeText(applicationContext, "Correo electrónico enviado", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: MessagingException) {
+            e.printStackTrace()
+            runOnUiThread {
+                Toast.makeText(applicationContext, "Error al enviar el correo electrónico", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
 private fun showPasswordDialog() {
     // Mostrar el cuadro flotante
