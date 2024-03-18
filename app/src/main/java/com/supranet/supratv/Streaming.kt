819eeb802095ef2226a9119023b5e72f1588526d
Epirex <estebanguzzo@gmail.com>
2024-03-18 00:41:50 -0300

    Initial streaming advertising feature

--- app/src/main/java/com/supranet/supratv/Streaming.kt
+++ app/src/main/java/com/supranet/supratv/Streaming.kt
@@ -1,60 +1,65 @@
 package com.supranet.supratv
 
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
-import com.airbnb.lottie.LottieAnimationView
-import java.util.*
+import androidx.webkit.WebViewClientCompat
 
 class Streaming : AppCompatActivity() {
 
-    private var timer: Timer? = null
+    private lateinit var videoView: VideoView
+    private lateinit var webView: android.webkit.WebView
+    private val handler = Handler()
+    private var webViewVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_streaming)
 
-        // Lottie time!
-        val lottieAnimationView = findViewById<LottieAnimationView>(R.id.lottieAnimationView)
-        lottieAnimationView.playAnimation()
-
-        // Ponemos un handler hasta que definamos una animacion final
-        Handler().postDelayed({
-            lottieAnimationView.cancelAnimation()
-            lottieAnimationView.visibility = View.GONE
-        }, 5000)
-
-        val videoView = findViewById<VideoView>(R.id.videoview)
-        val videoUri =
-            Uri.parse("https://live-01-02-eltrece.vodgc.net/eltrecetv/index.m3u8?PlaylistM3UCL")
+        videoView = findViewById(R.id.videoview)
+        val videoUri = Uri.parse("https://live-01-02-eltrece.vodgc.net/eltrecetv/index.m3u8?PlaylistM3UCL")
         videoView.setVideoURI(videoUri)
         videoView.start()
 
-        // Timer para el bucle entre actividades
-        if (timer == null) {
-            timer = Timer()
-            val task = object : TimerTask() {
-                var isStreamingActivityShowing = false
-                override fun run() {
-                    runOnUiThread {
-                        if (isStreamingActivityShowing) {
-                            val intent = Intent(this@Streaming, MainActivity::class.java)
-                            startActivity(intent)
-                            isStreamingActivityShowing = false
-                            timer?.cancel()
-                            timer = null
-                            finish()
-                        } else {
-                            isStreamingActivityShowing = true
-                        }
-                    }
-                }
+        // Webview settings
+        webView = android.webkit.WebView(this)
+        val layoutParams = FrameLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        layoutParams.gravity = Gravity.CENTER
+        webView.layoutParams = layoutParams
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClientCompat()
+
+        val container = findViewById<FrameLayout>(R.id.video_container)
+        container.addView(webView)
+        webView.visibility = View.GONE
+
+        startWebViewLoop()
+    }
+
+    private fun startWebViewLoop() {
+        handler.postDelayed({
+            if (webViewVisible) {
+                webView.visibility = View.GONE
+            } else {
+                webView.visibility = View.VISIBLE
+                webView.loadUrl("https://estebanguzzo.com.ar/publicidadcata/")
             }
-            timer?.schedule(task, 0, 1 * 60 * 1000)
-        }
+            webViewVisible = !webViewVisible
+            startWebViewLoop()
+        }, 30 * 1000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
     }
 }
