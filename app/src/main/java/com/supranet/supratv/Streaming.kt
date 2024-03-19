52a247780555ddd7481301f12a2a9d0d8db015c4
Epirex <estebanguzzo@gmail.com>
2024-03-19 11:02:25 -0300

    Initial support for controls player

--- app/src/main/java/com/supranet/supratv/Streaming.kt
+++ app/src/main/java/com/supranet/supratv/Streaming.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.view.Gravity
+import android.view.KeyEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
@@ -17,13 +18,19 @@ class Streaming : AppCompatActivity() {
     private lateinit var webView: android.webkit.WebView
     private val handler = Handler()
     private var webViewVisible = false
+    private var currentChannelIndex = 0
+    private val channels = listOf(
+        "https://live-01-02-eltrece.vodgc.net/eltrecetv/index.m3u8?PlaylistM3UCL",
+        "https://stmv6.voxtvhd.com.br/crossingtv/crossingtv/playlist.m3u8?PlaylistM3UCL",
+        "https://video06.logicahost.com.br/retroplustv/retroplustv/playlist.m3u8?PlaylistM3UCL",
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_streaming)
 
         videoView = findViewById(R.id.videoview)
-        val videoUri = Uri.parse("https://live-01-02-eltrece.vodgc.net/eltrecetv/index.m3u8?PlaylistM3UCL")
+        val videoUri = Uri.parse(channels[currentChannelIndex])
         videoView.setVideoURI(videoUri)
         videoView.start()
 
@@ -45,6 +52,38 @@ class Streaming : AppCompatActivity() {
         webView.visibility = View.GONE
 
         startWebViewLoop()
+    }
+
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+        when (event.keyCode) {
+            KeyEvent.KEYCODE_DPAD_RIGHT -> {
+                switchToNextChannel()
+                return true
+            }
+            KeyEvent.KEYCODE_DPAD_LEFT -> {
+                switchToPreviousChannel()
+                return true
+            }
+        }
+        return super.dispatchKeyEvent(event)
+    }
+
+    private fun switchToNextChannel() {
+        currentChannelIndex = (currentChannelIndex + 1) % channels.size
+        val videoUri = Uri.parse(channels[currentChannelIndex])
+        videoView.setVideoURI(videoUri)
+        videoView.start()
+    }
+
+    private fun switchToPreviousChannel() {
+        currentChannelIndex = if (currentChannelIndex == 0) {
+            channels.size - 1
+        } else {
+            currentChannelIndex - 1
+        }
+        val videoUri = Uri.parse(channels[currentChannelIndex])
+        videoView.setVideoURI(videoUri)
+        videoView.start()
     }
 
     private fun startWebViewLoop() {
