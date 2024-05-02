eeadb1ec5d048f7733f32806fa085f62917937ca
Epirex <estebanguzzo@gmail.com>
2024-05-02 17:45:46 -0300

    Fix Input Type for numbers in Streaming Settings
    Signed-off-by: Epirex <estebanguzzo@gmail.com>

--- app/src/main/java/com/supranet/supratv/SettingsStreaming.kt
+++ app/src/main/java/com/supranet/supratv/SettingsStreaming.kt
@@ -2,8 +2,10 @@ package com.supranet.supratv
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsStreaming : AppCompatActivity() {
@@ -27,6 +29,24 @@ class SettingsStreaming : AppCompatActivity() {
     class AdvertisingSettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.streaming_preferences, rootKey)
+
+            val baseAdvertisingTimePreference: EditTextPreference? = findPreference("base_advertising_time")
+            baseAdvertisingTimePreference?.setOnBindEditTextListener { editText ->
+                editText.inputType = InputType.TYPE_CLASS_NUMBER
+                editText.setSelection(editText.text.length)
+            }
+
+            val mixedStreamingTimePreference: EditTextPreference? = findPreference("mixed_streaming_time")
+            mixedStreamingTimePreference?.setOnBindEditTextListener { editText ->
+                editText.inputType = InputType.TYPE_CLASS_NUMBER
+                editText.setSelection(editText.text.length)
+            }
+
+            val mixedAdvertisingTimePreference: EditTextPreference? = findPreference("mixed_advertising_time")
+            mixedAdvertisingTimePreference?.setOnBindEditTextListener { editText ->
+                editText.inputType = InputType.TYPE_CLASS_NUMBER
+                editText.setSelection(editText.text.length)
+            }
         }
     }
 }
